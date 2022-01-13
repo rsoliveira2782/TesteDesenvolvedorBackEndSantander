f035a8c17c2624594f1dc0d76a40879df0821d5b
Rodrigo Oliveira <rodrigo.oliveira@synchro.com.br>
2022-01-12 21:56:22 -0300

    Commit inicial

---
+++ src/main/java/teste/desenvolvedor/backend/java/santander/TesteDesenvolvedorBackendJavaSantander/TesteDesenvolvedorBackendJavaSantanderApplication.java
@@ -0,0 +1,13 @@
+package teste.desenvolvedor.backend.java.santander.TesteDesenvolvedorBackendJavaSantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDesenvolvedorBackendJavaSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDesenvolvedorBackendJavaSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste/desenvolvedor/backend/java/santander/TesteDesenvolvedorBackendJavaSantander/TesteDesenvolvedorBackendJavaSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.desenvolvedor.backend.java.santander.TesteDesenvolvedorBackendJavaSantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDesenvolvedorBackendJavaSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
